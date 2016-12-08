ef1eaef7d1610a1f7670dcf2ff9be510692d33d4
fadyazmy <fadel@uwo.ca>
2016-12-08 09:57:50 -0500

    readme

---
+++ README.md
@@ -0,0 +1,28 @@
+#Twilio Starter Package
+
+##Set up
+
+1. Download this package
+2. Make an account on Twilio 
+3. Replace the Account Sid and AuthToken with your respective keys 
+4. Authenticate your personal number on Twilio
+5. Replace the TO and FROM numbers with your personal number and (free) Twilio number respectively  
+4. Move ngrok to $PATH with command `mv ngrok /usr/local/bin` 
+5. Run `node app.js` 
+6. Also run `ngrok http 1337`
+
+
+###Preface
+- The node server runs on port 1337, ngrok just gives you a public interface for external apps to use.
+
+
+## How to use
+
+- You can now access your server locally by going to `localhost:1337`
+- Or you can access the server from a public server via `http://820187c0.ngrok.io` as per the screenshot below ![alt tag] ()
+- Access `localhost:1337/pizzatext` to send yourself a simple pizza text
+- Access `localhost:1337/pizzacall` to receive a demo call =)
+
+##TODO
+- Easy way to upload xml file to change calling script
+- Integrate calling feature with texting to develop a complex app 
--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var express = require('express');
 
 var app = express();
 
-var accountSid = 'ACd7c06ebd2e690d16b1d0ca7b47bfae01';
-var authToken = 'd913d5b8ed7da636bd996e8d32eec699';
+var accountSid = 'YOUR-ACCOUNT-SID';
+var authToken = 'YOUR-ACCESS-TOKEN';
 
 var client = require('twilio')(accountSid, authToken);
 
@@ -14,17 +14,18 @@ app.get('/', function (req, res) {
 res.send("Hello world!")
 })
 
-var port = process.env.PORT || 8080;
-
-//The to numbers and the from numbers are fixed:
-//The to number is given from Twilio
-//The from number is the number authenticated by twilio
-//Though if you are a paid customer, you don't need to authenticate
+var port = process.env.PORT || 1337;
+/*
+ The to numbers and the from numbers are fixed
+ The to number is given from Twilio
+ The from number is the number authenticated by twilio
+ Though if you are a paid customer, you don't need to authenticate
+* */
 app.get('/pizzatext', function (req, res) {
   client.messages.create({
-    to: "+12269275775",
+    to: "+15196946103",
     from: "+12262701627",
-    body: "Hello I wanna buy a pizza please =) "
+    body: "Hello - wanna buy a pizza? =) "
   }, function(err, message) {
     console.log(message.sid);
   });
@@ -33,7 +34,7 @@ app.get('/pizzatext', function (req, res) {
 app.get('/pizzacall', function (req, res) {
 
   client.calls.create({
-    url: "http://demo.twilio.com/docs/voice.xml", // Needs to be public url?
+    url: "http://demo.twilio.com/docs/voice.xml", // Needs to be public url
     to: "+12269275775",
     from: "+12262701627"
   }, function(message) {
@@ -46,9 +47,6 @@ app.listen(port, function() {
   console.log('Our app is running on http://localhost:' + port);
 });
 
-//
-// http.createServer(app).listen(1338, function () {
-//   console.log("Express server listening on port 1338");
-// });
+
 
 module.exports = app;
